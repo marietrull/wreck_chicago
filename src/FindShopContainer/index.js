d02bedccd0b56c682d13085affd6663e883e5c87
marietrull <marietrull@gmail.com>
2018-06-15 16:57:10 -0500

    Added component for finding shops - FindShopContainer

---
+++ src/FindShopContainer/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class FindShopContainer extends Component{
+
+	constructor () {
+
+		super();
+
+	}
+
+
+	render () {
+
+
+		return (
+
+			<div> FindShopContainer </div>
+
+			)
+
+	}
+}
+
+
+export default FindShopContainer;
--- src/HomeContainer/index.js
+++ src/HomeContainer/index.js
@@ -5,6 +5,7 @@ import WreckStart from '../WreckStart';
 import FindWreckContainer from '../FindWreckContainer';
 import LogWreck from'../LogWreck';
 import MyWrecksContainer from '../MyWrecksContainer';
+import FindShopContainer from '../FindShopContainer';
 
 const HomeContainer = () => {
 		
@@ -17,6 +18,7 @@ const HomeContainer = () => {
 		  			<li><Link className='navItem' to="/FindAWreck">Wreck Finder</Link></li>
 		  			<li><Link className='navItem' to="/LogAWreck">Log a Wreck</Link></li>
 					<li><Link className='navItem' to="/MyWrecks">My Wrecks</Link></li>
+					<li><Link className='navItem' to="/FindAShop">Find a Shop</Link></li>
 					<li><a className='navItem' href="/logout">Logout</a></li>
 				</ul>
 
@@ -25,6 +27,7 @@ const HomeContainer = () => {
 					<Route exact path='/FindAWreck' component={FindWreckContainer} />
 					<Route exact path='/LogAWreck' component={LogWreck} />  
 					<Route exact path='/MyWrecks' component={MyWrecksContainer} />
+					<Route exact path='/FindAShop' component={FindShopContainer} />
 				</Switch>
 
 			</div>
